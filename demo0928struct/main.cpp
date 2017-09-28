417e55e283adcbbaaa7f98242228287304530bac
luckily248 <luck248@163.com>
2017-09-28 12:08:06 -0400

    Initial Commit

---
+++ demo0928struct/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  demo0928struct
+//
+//  Created by lucky on 9/28/17.
+//  Copyright © 2017 luck. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
